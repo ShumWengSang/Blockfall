a0c0ad938f71b9a5527f62e9ab35790df8a79f43
Shawn Lee <Prawnyman@gmail.com>
2016-04-04 20:42:37 +0800

    Added rotate but it's instant. Blocks fall. Need to make them align properly.

---
+++ Assets/Scripts/BlockMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BlockMovement : MonoBehaviour {
+
+	public Vector3 blockVel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		blockVel = GetComponent<Rigidbody>().velocity;
+		/*
+		if (blockVel.x == 0 && blockVel.y == 0) {
+			Vector3 positionVec = transform.position;
+			positionVec.x = (float)Math.Round(positionVec.x, MidpointRounding.AwayFromZero) / 2;
+			positionVec.y = (float)Math.Round(positionVec.x, MidpointRounding.AwayFromZero) / 2;
+
+			transform.position = positionVec;
+		}
+		*/
+
+		if ((Math.Abs (blockVel.x) < 0.01) && (Math.Abs (blockVel.y) < 0.01)) {
+			
+		}
+	}
+}
---
+++ Assets/Scripts/RotateGrid.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateGrid : MonoBehaviour {
+
+	public bool rotatingGrid;
+
+	public Transform targetRotation;
+
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rotatingGrid = false;
+		rotationSpeed = 0.001f;
+		targetRotation = transform;
+	}
+
+	void InitRotate()
+	{
+		rotatingGrid = true;
+		GameObject.FindGameObjectsWithTag ("WoodBlock");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (!rotatingGrid) {
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				Vector3 rotationVec = targetRotation.rotation.eulerAngles;
+				rotationVec.z = rotationVec.z + 90;
+				targetRotation.rotation = Quaternion.Euler (rotationVec);
+
+				rotatingGrid = true;
+			}
+
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
+				Vector3 rotationVec = targetRotation.rotation.eulerAngles;
+				rotationVec.z = rotationVec.z - 90;
+				targetRotation.rotation = Quaternion.Euler (rotationVec);
+
+				rotatingGrid = true;
+			}
+		} 
+		else {
+			float step = rotationSpeed * Time.deltaTime;
+
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation.rotation, 1.0f);
+
+			if (transform.rotation.z == targetRotation.rotation.z) {
+				rotatingGrid = false;
+				Mathf.RoundToInt (transform.rotation.z);
+			}
+		}
+
+
+	} //Update
+
+
+}
