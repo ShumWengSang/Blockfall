94b19925b647288b92d9c91b060d1841b115dcf7
Roland <ballwallcallall@gmail.com>
2016-04-07 13:55:37 +0800

    Added level transition.

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -2,8 +2,36 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour {
+    ScreenManager screenManager;
+    public Animator screenTransAnim; 
+    public float waitSeconds = 0.5f;
+    WaitForSeconds wait;
+    void Awake()
+    {
+        wait = new WaitForSeconds(waitSeconds);
+        screenManager = GetComponent<ScreenManager>();
+    }
+
+    void Start()
+    {
+        Debug.Log("Running this");
+        screenManager.OpenPanel(screenTransAnim);
+    }
+    void OnLevelWasLoaded(int index)
+    {
+        Debug.Log("Loading level");
+        screenManager.OpenPanel(screenTransAnim);
+    }
+
     public void ChangeScene(string scene)
     {
+        screenManager.CloseCurrent(scene);
+        StartCoroutine(loadNextScene(scene));
+    }
+
+    IEnumerator loadNextScene(string scene)
+    {
+        yield return wait;
         SceneManager.LoadSceneAsync(scene);
     }
 }
