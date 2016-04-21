3eedbbb92d269760be4308b7018cbd065b938338
Roland <ballwallcallall@gmail.com>
2016-04-21 17:28:38 +0800

    Split up pulsating script into its own script.

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -13,38 +13,13 @@ public class BlockMovement : MonoBehaviour {
 	public float fallSpeedThreshold; //Must exceed falling speed before shrinking collision box
     Rigidbody rb;
     BoxCollider box;
-    Tween theTween;
 
-    void OnEnable()
-    {
-        RotateGrid.OnFinishedRotating += OnStartFall;
-        RotateGrid.OnFinishedFalling += OnEndFall;
-    }
-
-    void OnDisable()
-    {
-        RotateGrid.OnFinishedRotating -= OnStartFall;
-        RotateGrid.OnFinishedFalling -= OnEndFall;
-    }
 	// Use this for initialization
 	void Start () {
         box = GetComponent<BoxCollider>();
         rb = GetComponent<Rigidbody>();
         origBoxColSize = box.size;
-        theTween = transform.DOBlendableScaleBy(new Vector3(0.05f,0.05f,0f), 5f).SetEase(Ease.InOutElastic).SetLoops(-1, LoopType.Yoyo);
 	}
-
-    void OnEndFall()
-    {
-        theTween.Play(); 
-    }
-
-    void OnStartFall()
-    {
-        theTween.Pause();
-        this.transform.localScale = Vector3.one;
-        SnapObject.SnapTheObject(this.transform);
-    }
 
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/Scripts/Wooden Blocks/Pulsating.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+public class Pulsating : MonoBehaviour {
+
+	// Use this for initialization
+    Tween theTween;
+
+    void OnEnable()
+    {
+        RotateGrid.OnFinishedRotating += OnStartFall;
+        RotateGrid.OnFinishedFalling += OnEndFall;
+    }
+
+    void OnDisable()
+    {
+        RotateGrid.OnFinishedRotating -= OnStartFall;
+        RotateGrid.OnFinishedFalling -= OnEndFall;
+    }
+
+	void Start () {
+        theTween = transform.DOBlendableScaleBy(new Vector3(0.05f, 0.05f, 0f), 5f).SetEase(Ease.InOutElastic).SetLoops(-1, LoopType.Yoyo);
+	}
+
+    void OnEndFall()
+    {
+        theTween.Play();
+    }
+
+    void OnStartFall()
+    {
+        theTween.Pause();
+        this.transform.localScale = Vector3.one;
+        SnapObject.SnapTheObject(this.transform);
+    }
+
+}
