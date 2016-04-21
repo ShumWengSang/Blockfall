852f705023bcd23a6a8a60fea6b0480f555071d9
Shawn Lee <Prawnyman@gmail.com>
2016-04-21 13:56:43 +0800

    Added game element textures. Also gave game system a tag of 'GameController' so it can be easily referenced in scripts

---
+++ Assets/ChangeWallTexture.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeWallTexture : MonoBehaviour {
+
+    public Material[] wallTextures;
+
+    GameObject GameSystem;
+
+	// Use this for initialization
+	void Start () {
+        GameSystem = GameObject.FindGameObjectWithTag("GameController");
+
+        GetComponent<MeshRenderer>().material = wallTextures[GameSystem.GetComponent<ScoreSystem>().World - 1];
+    }
+}
