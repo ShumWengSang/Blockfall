14d9d35e25a5ed7dc08328453ceaeda6ca6e03e5
Roland <ballwallcallall@gmail.com>
2016-05-03 16:45:50 +0800

    Run Time timescale script.

---
+++ Assets/Scripts/TimeScaleController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScaleController : MonoBehaviour {
+
+    public float timeScale = 2.5f;
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = timeScale;
+	}
+    
+    void OnEnable()
+    {
+        GoalChecker.OnFinishedGame += OnFinishedGame;
+    }
+
+    void OnDisable()
+    {
+        GoalChecker.OnFinishedGame -= OnFinishedGame;
+    }
+
+    void OnFinishedGame()
+    {
+        timeScale = 1f;
+    }
+}
