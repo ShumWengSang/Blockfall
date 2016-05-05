d04bac521c60c9082880dd04910e73cd7b384d5a
Shawn Lee <Prawnyman@gmail.com>
2016-05-05 16:19:06 +0800

    Portal element and 4-1 and 4-2

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+    public GameObject OtherPortal;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.position.y - transform.position.y > 0) //Check if above
+            other.transform.position = OtherPortal.transform.position;
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
