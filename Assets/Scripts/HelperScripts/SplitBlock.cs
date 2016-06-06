ee3d8f7466943bc8b7a726b6662bfd3a20d2e7d3
ballwallcallall <ballwallcallall@gmail.com>
2016-06-06 14:45:11 +0800

    Added ability to read and parse from answer txt files. Not too sure what to do with the powerups. Currently working on last powerup, which is moving of block. Making a script that will split a block up into its respective sizes.

---
+++ Assets/Scripts/HelperScripts/SplitBlock.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using AdvancedInspector;
+public class SplitBlock : MonoBehaviour {
+    //Usually we want this running on editor time.
+    //But just in case we run this at start as wel.
+
+
+    public GameObject BasePrefab;
+
+    [Inspect, Method(AdvancedInspector.MethodDisplay.Invoke)]
+    void SplitTheBlock()
+    {
+        int FullScaleX = (int)transform.localScale.x;
+        int FullScaleY = (int)transform.localScale.y;
+        int HalfScaleX = FullScaleX % 2 == 0 ? FullScaleX / 2 : (FullScaleX - 1) / 2;
+        int HalfScaleY = FullScaleY % 2 == 0 ? FullScaleY / 2 : (FullScaleY - 1) / 2;
+        Debug.Log("Half ScaleX is " + HalfScaleX + " and HalfScaleY is " + HalfScaleY);
+
+        for (int i = 0; i < HalfScaleX / 2; i++)
+        {
+            GameObject obj = GameObject.Instantiate(BasePrefab, );
+        }
+
+        for (int i = 0; i < HalfScaleX / 2; i++)
+        {
+
+        }
+
+        for (int i = 0; i < transform.localScale.y; i++)
+        {
+
+        }
+    }
+
+    void Start()
+    {
+        SplitTheBlock();
+    }
+}
---
+++ Assets/Scripts/PowerUps/ReadAnswerFile.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using AdvancedInspector;
+using System.Collections.Generic;
+using System.IO;
+public class ReadAnswerFile : MonoBehaviour {
+
+    public List<string> List_Answer = new List<string>();
+
+    void OnEnable()
+    {
+        ScoreSystem.OnParsed += OnLevelParsed;
+    }
+
+    void OnDisable()
+    {
+        ScoreSystem.OnParsed -= OnLevelParsed;
+    }
+    void OnLevelParsed(int world, int level)
+    {
+        ReadFile(world, level);
+    }
+
+
+    void ReadFile(int world, int level)
+    {
+        int count = 0;
+        string line;
+
+        StreamReader sr = new StreamReader("Answers/Level" + world.ToString() + "-" + level.ToString() + ".txt");
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            if (line.Contains("Left"))
+            {
+                List_Answer.Add("Left");
+            }
+            else
+            {
+                List_Answer.Add("Right");
+            }
+            count++;
+        }
+        
+
+        foreach(string direction in List_Answer)
+        {
+            Debug.Log(direction);
+        }
+    }	
+}
